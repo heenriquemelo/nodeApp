1b5b7b69a4aca6790a9809d3e40afd2d7e3ef8e2
Henrique Melo <hpa.de.melo@gmail.com>
2016-03-23 21:21:11 -0400

    Test 5

--- models/users.js
+++ models/users.js
@@ -10,7 +10,7 @@ var db = mongoose.connection; // ??
 
 // Establishes connection to db. If run locally, u must create a new terminal window, and run the db from there.
 // The command "mongod --port <port number>" takes care of the work.
-// mongoose.connect('mongodb://localhost:12345');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:12345');
 
 // Once the connection is made, execute the callback.
 db.once('open', function() {
@@ -34,24 +34,24 @@ var henrique = new User({name: 'Henrique'});
 
 // Save the instance of the model to the db. 
 
-/*henrique.save(function(err, henrique) {
+henrique.save(function(err, henrique) {
   if (err) {
     console.error(err);
   } else {
     console.log(henrique.name + ' has been added to the database!');
   }
 
-});*/
+});
 
 // Read data. In this case, display all db items that are instances of the User model.
 
-/*User.find({}, function(err, allUsers) {
+User.find({}, function(err, allUsers) {
   if (err) {
     console.error(err);
   } else {
     console.log('These are all users:\n' + allUsers);
   }
-});*/
+});
 
 // Assigns the value of require('users.js') in the main file to User.
 module.exports = User;
